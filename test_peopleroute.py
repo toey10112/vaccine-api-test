03cd8e91b56f2c05a90f2bb3de89440dec4da4d9
Suchon Prasert <58396402+toey10112@users.noreply.github.com>
2021-11-18 22:59:34 +0700

    fix wrong docstring

--- test_peopleroute.py
+++ test_peopleroute.py
@@ -26,55 +26,55 @@ schema = {
 
 
 class PeopleRouteTest(unittest.TestCase):
-    """
-    Test ID: 1
-    Test get all people work normally
-    """
 
     def test_get_all_people(self):
+        """
+
+        Test ID: 1
+        Test get all people work normally
+        """
         api = URL + "/all"
         res = requests.get(api)
         self.assertEqual(200, res.status_code)
 
-    """
-    Test ID: 2
-    Test get people by date work normally 
-    """
-
     def test_get_people_by_date(self):
+        """
 
+          Test ID: 2
+          Test get people by date work normally
+        """
         api = URL + "/by_date/23-10-2021"
         res = requests.get(api)
         self.assertEqual(200, res.status_code)
         self.assertEqual(schema, res.json())
 
-    """
-    Test ID: 3
-    Test get people by using wrong format date
-    """
-
     def test_get_people_by_wrong_date_format(self):
+        """
+
+        Test ID: 3
+        Test get people by using wrong format date
+        """
         api = URL + "/by_date/10-20-2021"
         # date format: m/d/y
         res = requests.get(api)
         self.assertEqual(404, res.status_code)
 
-    """
-    Test ID: 4
-    Test get people by date but without date input 
-    """
-
     def test_get_people_without_date(self):
+        """
+
+        Test ID: 4
+        Test get people by date but without date input
+        """
         api = URL + "/by_date/"
         res = requests.get(api)
         self.assertEqual(404, res.status_code)
 
-    """
-    Test ID: 5 
-    Test get people by date without data
-    """
-
     def test_get_people_by_date_without_data(self):
+        """
+
+        Test ID: 5
+        Test get people by date without data
+        """
         api = URL + "/by_date/24-10-2021"
         # Database doesn't have people appointment on this day
         res = requests.get(api)
@@ -82,36 +82,36 @@ class PeopleRouteTest(unittest.TestCase):
         self.assertEqual("no date included", res.json())
         # So it should be empty
 
-    """
-    Test ID: 6
-    Test get people by date with future date 
-    """
-
     def test_get_people_by_future_date(self):
+        """
+
+        Test ID: 6
+        Test get people by date with future date
+        """
         api = URL + "/by_date/24-10-2150"
         # Future date
         res = requests.get(api)
         self.assertEqual(202, res.status_code)
         self.assertEqual("", res.text)
 
-    """
-    Test ID: 7
-    Test delete people by date but that date doesn't have data
-    """
-
     def test_delete_people_by_date_without_data(self):
+        """
+
+        Test ID: 7
+        Test delete people by date but that date doesn't have data
+        """
         api = URL + "/by_date/25-09-2021"
         # Database doesn't have people in this date
         res = requests.delete(api)
         self.assertEqual(202, res.status_code)
         self.assertEqual("no date included", res.json())
 
-    """
-    Test ID: 8
-    Test delete people by date
-    """
-
     def test_delete_people_by_date(self):
+        """
+
+        Test ID: 8
+        Test delete people by date
+        """
         api = URL + "/by_date/23-10-2021"
         res = requests.delete(api)
         self.assertEqual(200, res.status_code)
